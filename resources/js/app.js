a7e41328d8b4e886c79decc9c1b6c56041f43519
BXium <jeabanggud@addu.edu.ph>
2024-05-29 09:02:12 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m.js';
 
 // Import Font Awesome packages
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -21,6 +22,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(ZiggyVue, Ziggy)
             .component('font-awesome-icon', FontAwesomeIcon) // Register FontAwesomeIcon as a component
             .mount(el);
     },
